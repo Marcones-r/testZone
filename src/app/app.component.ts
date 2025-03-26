0ea4697e568485e3dcede814bd31395cbeb84e50
Marcones-r <markonespro@gmail.com>
2025-03-26 16:53:32 +0100

    liaison de propriétes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import {Component} from '@angular/core';
-import { TestsComponent } from './tests/tests.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: true,
-  imports: [TestsComponent],
+  styleUrls: ['app.component.css'],
+  template: `
+  <!--  -->
+  <!-- <button [disabled]="isDisabled">Cliquez-moi</button> -->
+  <!-- <p [style.fontSize.px]="fontSize">Texte avec taille dynamique</p> -->
+  `,
 })
 export class AppComponent {
-  
-
+  // 
+  // isDisabled = true; /* isEditable est la "propriéte de classe et true est la "valeur" */
+  // fontSize = 66;
 }
+
+// Lien du l'exercice : https://angular.dev/tutorials/learn-angular/6-property-binding
+
+
+/*
+Liaison de propriétés dans Angular
+edit
+La liaison de propriétés dans Angular vous permet de définir des valeurs pour les propriétés des éléments HTML, des composants Angular et plus encore.
+
+Utilisez la liaison de propriétés pour définir dynamiquement les valeurs des propriétés et des attributs. Vous pouvez notamment activer/désactiver des boutons, définir des chemins d'accès aux images par programmation et partager des valeurs entre composants.
+*/
